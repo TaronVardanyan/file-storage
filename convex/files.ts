51b7d078571d6d4cbd16cfd73c663ce940f0f91b
TaronVardanyan <tarokavardanyan@gmail.com>
2024-05-07 12:42:46 +0400

    add initial convex mutation and queries

---
+++ convex/files.ts
@@ -0,0 +1,32 @@
+import { mutation, query } from "./_generated/server";
+import { v, ConvexError } from "convex/values";
+
+export const createFile = mutation({
+    args: {
+       name: v.string(),
+    },
+    async handler(ctx, args) {
+        const identity = await ctx.auth.getUserIdentity();
+        
+        if(!identity) {
+            throw new ConvexError("Unauthorized");
+        }
+        
+        await ctx.db.insert("files", {
+            name: args.name,
+        })
+    }
+});
+
+export const  getFiles = query({
+    args: {},
+    async handler(ctx, args) {
+        const identity = await ctx.auth.getUserIdentity();
+                
+        if(!identity) {
+           throw new ConvexError("Unauthorized");
+        }
+        
+        return ctx.db.query("files").collect();
+    }
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {useMutation, useQuery} from "convex/react";
+import { api } from "../../convex/_generated/api";
+
 export default function LandingPage() {
+  const createFile = useMutation(api.files.createFile);
+  const files = useQuery(api.files.getFiles);
+
+  const handleClick = async () => {
+    await createFile({
+      name: "hello world",
+    })
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home page
+        <Button onClick={handleClick}>click me</Button>
+        {files?.map(file => <div key={file._id}>{file.name}</div>)}
     </main>
   );
 }
