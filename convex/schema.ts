b4775272e29c000dbf38036bd11f9fcc9008db77
TaronVardanyan <tarokavardanyan@gmail.com>
2024-05-07 14:48:27 +0400

    add convex schema changes

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,5 +2,5 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-    files: defineTable({ name: v.string() }),
+    files: defineTable({ name: v.string(), orgId: v.string() }).index("by_orgId", ["orgId"]),
 });
