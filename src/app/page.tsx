0ddb7a383ea8c11866ba4174aa9ffcee404ca890
TaronVardanyan <tarokavardanyan@gmail.com>
2024-05-15 17:32:31 +0400

    add empty state views

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import UploadButton from '@/components/upload-button';
 import FileCard from '@/components/file-card';
 import { useOrganization, useUser } from '@clerk/nextjs';
 import { api } from '../../convex/_generated/api';
+import Image from 'next/image';
+import { Loader2 } from 'lucide-react';
 
 export default function LandingPage() {
   const { organization } = useOrganization();
@@ -18,9 +20,22 @@ export default function LandingPage() {
         <h1 className="text-4xl font-bold">Your files</h1>
         <UploadButton />
       </div>
-      <div className="grid grid-cols-4 gap-4">
-        {files?.map((file) => <FileCard key={file._id} file={file} />)}
-      </div>
+      {files === undefined && (
+        <div className="flex items-center justify-center">
+          <Loader2 className="h-20 w-20 animate-spin text-gray-500" />
+        </div>
+      )}
+      {files?.length === 0 ? (
+        <div className="flex flex-col items-center justify-center gap-8">
+          <Image src="./empty.svg" alt="Empty" width={200} height={200} />
+          <div className="text-center text-2xl">You have no files, upload one.</div>
+          <UploadButton />
+        </div>
+      ) : (
+        <div className="grid grid-cols-4 gap-4">
+          {files?.map((file) => <FileCard key={file._id} file={file} />)}
+        </div>
+      )}
     </main>
   );
 }
