ea7f07385482a52780195a4d3fd57ae452cd827b
TaronVardanyan <tarokavardanyan@gmail.com>
2024-06-14 17:41:54 +0400

    add updates for upload user data

--- convex/users.ts
+++ convex/users.ts
@@ -1,14 +1,41 @@
-import { internalMutation, QueryCtx, MutationCtx } from './_generated/server';
+import { internalMutation, QueryCtx, MutationCtx, query } from './_generated/server';
 import { ConvexError, v } from 'convex/values';
 
 export const createUser = internalMutation({
   args: {
     tokenIdentifier: v.string(),
+    name: v.string(),
+    image: v.string(),
   },
   async handler(ctx, args) {
     await ctx.db.insert('users', {
       tokenIdentifier: args.tokenIdentifier,
       orgIds: [],
+      name: args.name,
+      image: args.image,
+    });
+  },
+});
+
+export const updateUser = internalMutation({
+  args: {
+    tokenIdentifier: v.string(),
+    name: v.string(),
+    image: v.string(),
+  },
+  async handler(ctx, args) {
+    const user = await ctx.db
+      .query('users')
+      .withIndex('by_tokenIdentifier', (q) => q.eq('tokenIdentifier', args.tokenIdentifier))
+      .first();
+
+    if (!user) {
+      throw new ConvexError('User not found');
+    }
+
+    await ctx.db.patch(user._id, {
+      name: args.name,
+      image: args.image,
     });
   },
 });
@@ -61,5 +88,17 @@ export const updateRoleInOrgForUser = internalMutation({
     await ctx.db.patch(user._id, {
       orgIds: user.orgIds,
     });
+  },
+});
+
+export const getUserProfile = query({
+  args: { userId: v.id('users') },
+  async handler(ctx, args) {
+    const user = await ctx.db.get(args.userId);
+
+    return {
+      name: user?.name,
+      image: user?.image,
+    };
   },
 });
